b6b5e82143505e8bdc61c1b1e94e34f275159a88
JesusGavilan <jegavilgar@gmail.com>
2020-04-13 19:18:30 +0200

    added simple DAG

---
+++ airflow/dags/simple.py
@@ -0,0 +1,27 @@
+import datetime
+from airflow import models
+from airflow.operators import bash_operator
+from airflow.operators import python_operator
+
+default_dag_args = {
+    'start_date': datetime.datetime(2018, 1, 1)
+}
+
+with models.DAG(
+        'composer_sample_greeting',
+        schedule_interval=datetime.timedelta(days=1),
+        default_args=default_dag_args) as dag:
+    def greeting():
+        import logging
+        logging.info('Hello World!')
+
+
+    hello_python = python_operator.PythonOperator(
+        task_id='hello',
+        python_callable=greeting)
+
+    goodbye_bash = bash_operator.BashOperator(
+        task_id='bye',
+        bash_command='echo Goodbye')
+
+    hello_python >> goodbye_bash
